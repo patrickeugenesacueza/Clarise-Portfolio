f4252c2d087b384dcb2bdc12b24493e2d75b53bb
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-01-03 05:13:44 +0800

    Develop: SkillSection.js

--- portfolio/src/components/pages/AboutMeSection.js
+++ portfolio/src/components/pages/AboutMeSection.js
@@ -18,16 +18,16 @@ export default function AboutMeSection() {
         </div>
         <div className="w-1/2 mx-auto md:text-left md:ml-11 mt-4 md:mt-0">
           <div className="text-[3rem] font-bold text-[#5f2568]">About Me</div>
-          <div className="sm: text-justify font-[sans-serif] text-[auto]">
+          <div className="sm: text-justify font-poppins font-[100]">
             Hello there! I'm Clarise Ann T. Duque, a driven and accomplished
             individual with a passion for financial management. I proudly earned
             my Bachelor of Science degree in Financial Management from
             Pamantasan ng Lungsod ng Maynila, where I graduated with the
-            prestigious distinction of Magna Cum Laude.  <br></br><br></br>Throughout my studies, I honed my
-            analytical skills, strategic thinking, and financial acumen,
-            preparing me to excel in the dynamic world of finance. I am eager to
-            leverage my education and skills to contribute meaningfully to the
-            financial sector.
+            prestigious distinction of Magna Cum Laude. <br></br>
+            <br></br>Throughout my studies, I honed my analytical skills,
+            strategic thinking, and financial acumen, preparing me to excel in
+            the dynamic world of finance. I am eager to leverage my education
+            and skills to contribute meaningfully to the financial sector.
           </div>
           <div>
             <button className="px-4 py-2 bg-[#b37eb5] mt-3.5 font-bold font-maven text-[#fff] rounded-3xl mx-auto border-2 border-[#b37eb5] hover:bg-[#fff] hover:text-[#b37eb5] duration-500 md:mx-0 2xl:text-[1.5rem] 2xl:px-6 2xl:py-4 2xl:rounded-[4em]">
--- portfolio/src/components/pages/SkillSection.js
+++ portfolio/src/components/pages/SkillSection.js
@@ -3,9 +3,57 @@ import React from "react";
 export default function SkillSection(){
     return (
       <>
-        <div className="py-5 z-0 bg-[#fff] w-full h-[auto] md:h-[90vh] lg:h-[90vh] 2xl:[50vh] px-0 text-center md:px-[10.5rem] lg:px-[10.5rem]">
-          
+        <div className="py-[5rem] z-0 bg-[#fff] w-full h-[70vh] md:h-[90vh] lg:h-[90vh] px-[2em] md:px-[10.5rem]">
+          <div className="item-center text-center">
+            <div>
+              <span className="font-poppins font-medium text-[15px] text-[#9b9696]">
+                Skills
+              </span>
+            </div>
+            <div>
+              <span className="font-poppins font-bold text-[30px] text-[#5f2568]">
+                Why Choose me
+              </span>
+            </div>
+            <div className="mt-5">
+              <span className="font-poppins font-[100] text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl sm: text-justify">
+                I humbly present myself, marked by the distinction of Magna Cum
+                Laude in BS Financial Management, <br></br>
+                with a genuine commitment to contributing modest yet meaningful
+                insights in any professional endeavor.
+              </span>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-12">
+              <div className="bg-[#fff] border-[.50px] border-[#b2bbc3] p-24 rounded-md text-center hover:text-white hover:bg-[#9b6c9a] hover:duration-500">
+                <div>
+                  <ion-icon name="analytics-outline" size="large"></ion-icon>
+                </div>
+                <div className="">Analytical</div>
+              </div>
+              <div className="bg-[#fff] border-[.50px] border-[#b2bbc3] p-24 rounded-md text-center hover:text-white hover:bg-[#9b6c9a] hover:duration-500">
+                <div className="">
+                  <ion-icon
+                    name="chatbubble-ellipses-outline"
+                    size="large"
+                  ></ion-icon>
+                </div>
+                <div className="">Communicative</div>
+              </div>
+              <div className="bg-[#fff] border-[.50px] border-[#b2bbc3] p-24 rounded-md text-center hover:text-white hover:bg-[#9b6c9a] hover:duration-500">
+                <div>
+                  <ion-icon name="bulb-outline" size="large"></ion-icon>
+                </div>
+                <div>Strategic</div>
+              </div>
+              <div className="bg-[#fff] border-[.50px] border-[#b2bbc3] p-24 rounded-md text-center hover:text-white hover:bg-[#9b6c9a] hover:duration-500">
+                <div>
+                  <ion-icon name="bar-chart-outline" size="large"></ion-icon>
+                </div>
+                <div>Ethical</div>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
-}
+}   
