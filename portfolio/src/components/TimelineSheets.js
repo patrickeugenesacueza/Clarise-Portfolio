d0e3231f73528db372156343f63955883f8b96d4
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-01-05 01:02:16 +0800

    Create TimelineSheet.js

---
+++ portfolio/src/components/TimelineSheets.js
@@ -0,0 +1,31 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Account Payables Intern",
+    location: "PPC, QUEZON CITY",
+    description: "Description",
+    button: "Button",
+    date: "August 2016 - present",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Front end developer",
+    location: "Pasay",
+    description: "Type type lang",
+    button: "Button",
+    date: "August 2016 - present",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "Front end developer",
+    location: "Pasay",
+    description: "Type type lang",
+    button: "Button",
+    date: "August 2016 - present",
+    icon: "work",
+  },
+];
+
+export default timelineElements;
