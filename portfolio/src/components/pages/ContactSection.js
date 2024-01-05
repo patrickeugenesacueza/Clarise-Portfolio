5ec16aa9f7b4aa69774f6ce180384042688d4003
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-01-06 05:11:25 +0800

    Update: ContactSection.js
    Install: ContactSEction / Install npm i -S @react-google-maps/api package #59

--- portfolio/src/components/pages/ContactSection.js
+++ portfolio/src/components/pages/ContactSection.js
@@ -3,6 +3,18 @@ import React from "react";
 
 export default function ContactSection(){
     return (
-      <div className="py-[7rem] b z-0 bg-[#c03636] w-full h-[90vh] md:h-[90vh] lg:h-[90vh] xl:h-[90vh] 2xl:h-[90vh] px-[2em] md:px-[10.5rem]"></div>
+      <div className="pt-[3rem] b z-0 bg-[#fff] w-full h-[90vh] md:h-[90vh] lg:h-[120vh] xl:h-[110vh] 2xl:h-[90vh] px-[2em] md:px-[0]">
+        <div className="h-1/2 w-full bg-[#fff] px-[2em] md:px-[10.5em]">
+          <div className="text-center font-poppins font-medium text-[15px] text-[#9b9696]">
+            Contact
+          </div>
+          <div className="text-center">
+            <span className="font-poppins font-bold text-[30px] text-[#5f2568]">
+              Get In Touch With Me
+            </span>
+          </div>
+        </div>
+        <div className="h-1/2 w-full bg-black"></div>
+      </div>
     );
 }
