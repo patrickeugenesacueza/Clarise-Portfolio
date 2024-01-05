2e3b53d56e32c5efc54c09c1f0e6449d33ee1c2b
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-01-06 04:35:59 +0800

    Update: ContactSection.js
    Develop: ContactSection / Create a contact section component #56

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import HeroSection from "./components/pages/HeroSection.js";
 import AboutMeSection from "./components/pages/AboutMeSection.js";
 import SkillSection from "./components/pages/SkillSection.js";
 import ExperienceSection from "./components/pages/ExperienceSection.js";
+import ContactSection from "./components/pages/ContactSection.js";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <AboutMeSection/>
         <SkillSection/>
         <ExperienceSection/>
+        <ContactSection/>
       </div>
     </>
   );
---
+++ portfolio/src/components/pages/ContactSection.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function ContactSection(){
+    return (
+      <div className="py-[7rem] b z-0 bg-[#c03636] w-full h-[90vh] md:h-[90vh] lg:h-[90vh] xl:h-[90vh] 2xl:h-[90vh] px-[2em] md:px-[10.5rem]"></div>
+    );
+}
