280d0db37d725aded06d3bff5551c5e9af921eb6
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-01-14 19:54:58 +0800

    Update: AchievementSection.js
    Created a header for achievementSection

--- portfolio/src/components/pages/AchievementSection.js
+++ portfolio/src/components/pages/AchievementSection.js
@@ -6,206 +6,221 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import cert1 from "../../image/cert1.png"
+import cert1 from "../../image/cert1.png";
 import cert2 from "../../image/cert2.jpg";
 import cert3 from "../../image/cert3.jpg";
 import cert4 from "../../image/cert4.png";
 
 export default function AchievementSection() {
   return (
-    <div className="bg-white h-[auto] md:h-[120vh] lg:h-[auto] py-[5rem] px-0 md:px-[10.5rem] lg:text-start w-full flex items-center justify-center ">
-      <Grid container spacing={3} className="mb-4">
-        {/* 1st certificate */}
-        <Grid item xs={12} md={4} className="flex justify-center">
-          <Card sx={{ maxWidth: 345 }} className="shadow-2xl">
-            <CardMedia
-              sx={{ height: 140 }}
-              image={cert2}
-              title="Gawad Karunungan"
-            />
-            <CardContent className="bg-[#b37eb5]">
-              <Typography gutterBottom variant="h5" component="div">
-                Semester award: Gawad Karunungan
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                The Gawad Karunungan Award is a prestigious recognition bestowed
-                upon an outstanding student in the Bachelor of Science in
-                Financial Management program.
-              </Typography>
-            </CardContent>
-            <CardActions className="bg-[#b37eb5]">
-              <a
-                href="https://drive.google.com/file/d/122Wz4RRzYc6OQiOedPNwBpBTTYLhxOXv/view?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button size="small">VIEW</Button>
-              </a>
-            </CardActions>
-          </Card>
-        </Grid>
+    <div className="bg-white h-[auto] md:h-[120vh] lg:h-[auto] py-[5rem] px-0 md:px-[10.5rem] ">
+      <div className="item-center text-center mb-10">
+        <div>
+          <span className="font-poppins font-medium text-[15px] text-[#9b9696]">
+            Achievements
+          </span>
+        </div>
+        <div>
+          <span className="font-poppins font-bold text-[30px] text-[#5f2568]">
+            Awards and Certifications
+          </span>
+        </div>
+      </div>
+      <div className="lg:text-start w-full flex items-center justify-center">
+        <Grid container spacing={3} className="mb-4">
+          {/* 1st certificate */}
+          <Grid item xs={12} md={4} className="flex justify-center">
+            <Card sx={{ maxWidth: 345 }} className="shadow-2xl">
+              <CardMedia
+                sx={{ height: 140 }}
+                image={cert2}
+                title="Gawad Karunungan"
+              />
+              <CardContent className="bg-[#b37eb5]">
+                <Typography gutterBottom variant="h5" component="div">
+                  Semester award: Gawad Karunungan
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  The Gawad Karunungan Award is a prestigious recognition
+                  bestowed upon an outstanding student in the Bachelor of
+                  Science in Financial Management program.
+                </Typography>
+              </CardContent>
+              <CardActions className="bg-[#b37eb5]">
+                <a
+                  href="https://drive.google.com/file/d/122Wz4RRzYc6OQiOedPNwBpBTTYLhxOXv/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button size="small">VIEW</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Grid>
 
-        {/* 2nd certificate */}
-        <Grid item xs={12} md={4} className="flex justify-center">
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image={cert1}
-              title="How-to-Series: Building a Path towards Financial Prowess"
-            />
-            <CardContent className="bg-[#b37eb5]">
-              <Typography gutterBottom variant="h5" component="div">
-                How-to-Series: Building a Path towards Financial Prowess
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                The "How-to-Series: Building a Path towards Financial Prowess"
-                is a comprehensive guide designed to empower individuals on
-                their journey to financial mastery.
-              </Typography>
-            </CardContent>
-            <CardActions className="bg-[#b37eb5]">
-              <a
-                href="https://drive.google.com/file/d/12K1N2rutDGOTq6ML6brOGVmoGQysE6yw/view?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button size="small">VIEW</Button>
-              </a>
-            </CardActions>
-          </Card>
-        </Grid>
+          {/* 2nd certificate */}
+          <Grid item xs={12} md={4} className="flex justify-center">
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={cert1}
+                title="How-to-Series: Building a Path towards Financial Prowess"
+              />
+              <CardContent className="bg-[#b37eb5]">
+                <Typography gutterBottom variant="h5" component="div">
+                  How-to-Series: Building a Path towards Financial Prowess
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  The "How-to-Series: Building a Path towards Financial Prowess"
+                  is a comprehensive guide designed to empower individuals on
+                  their journey to financial mastery.
+                </Typography>
+              </CardContent>
+              <CardActions className="bg-[#b37eb5]">
+                <a
+                  href="https://drive.google.com/file/d/12K1N2rutDGOTq6ML6brOGVmoGQysE6yw/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button size="small">VIEW</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Grid>
 
-        {/* 3rd certificate */}
-        <Grid item xs={12} md={4} className="flex justify-center">
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image={cert3}
-              title="Overall Top 2"
-            />
-            <CardContent className="bg-[#b37eb5]">
-              <Typography gutterBottom variant="h5" component="div">
-                Phoenix Awards: Overall Top 2 (GWA 1.3125)
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                The Gawad Karunungan Award is a prestigious recognition bestowed
-                upon an outstanding student in the Bachelor of Science in
-                Financial Management program.
-              </Typography>
-            </CardContent>
-            <CardActions className="bg-[#b37eb5]">
-              <a
-                href="https://drive.google.com/file/d/1240IuwpNxauhr3G-aCS4rY76-v2fS7Sc/view?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button size="small">VIEW</Button>
-              </a>
-            </CardActions>
-          </Card>
-        </Grid>
+          {/* 3rd certificate */}
+          <Grid item xs={12} md={4} className="flex justify-center">
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={cert3}
+                title="Overall Top 2"
+              />
+              <CardContent className="bg-[#b37eb5]">
+                <Typography gutterBottom variant="h5" component="div">
+                  Phoenix Awards: Overall Top 2 (GWA 1.3125)
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Achieving the title of Overall Top 2 with a remarkable General
+                  Weighted Average (GWA) of 1.3125 in the Bachelor of Science in
+                  Financial Management program.
+                </Typography>
+              </CardContent>
+              <CardActions className="bg-[#b37eb5]">
+                <a
+                  href="https://drive.google.com/file/d/1240IuwpNxauhr3G-aCS4rY76-v2fS7Sc/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button size="small">VIEW</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Grid>
 
-        {/* 4th certificate */}
-        <Grid item xs={12} md={4} className="flex justify-center">
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://udemy-certificate.s3.amazonaws.com/image/UC-3521c70d-7f8c-4c4c-a5df-bc5bf0848414.jpg?v=1690617477000"
-              title="green iguana"
-              loading="lazy"
-            />
+          {/* 4th certificate */}
+          <Grid item xs={12} md={4} className="flex justify-center">
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://udemy-certificate.s3.amazonaws.com/image/UC-3521c70d-7f8c-4c4c-a5df-bc5bf0848414.jpg?v=1690617477000"
+                title="green iguana"
+                loading="lazy"
+              />
 
-            <CardContent className="bg-[#b37eb5]">
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                className="font-bold"
-              >
-                Financial Analysis & Modeling | Automobile Section
-              </Typography>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                className="font-bold"
-              >
-                The "Financial Analysis & Modeling | Automobile Section" course
-                delves into the intricacies of financial analysis and modeling
-                within the context of the automotive industry.
-              </Typography>
-            </CardContent>
-            <CardActions className="bg-[#b37eb5]">
-              <a
-                href="https://www.udemy.com/certificate/UC-3521c70d-7f8c-4c4c-a5df-bc5bf0848414/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button size="small">VIEW</Button>
-              </a>
-            </CardActions>
-          </Card>
-        </Grid>
+              <CardContent className="bg-[#b37eb5]">
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="font-bold"
+                >
+                  Financial Analysis & Modeling | Automobile Section
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  className="font-bold"
+                >
+                  The "Financial Analysis & Modeling | Automobile Section"
+                  course delves into the intricacies of financial analysis and
+                  modeling within the context of the automotive industry.
+                </Typography>
+              </CardContent>
+              <CardActions className="bg-[#b37eb5]">
+                <a
+                  href="https://www.udemy.com/certificate/UC-3521c70d-7f8c-4c4c-a5df-bc5bf0848414/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button size="small">VIEW</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Grid>
 
-        {/* 4th certificate */}
-        <Grid item xs={12} md={4} className="flex justify-center">
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://udemy-certificate.s3.amazonaws.com/image/UC-977ceee6-810a-44e2-ba77-45335bfbb081.jpg?v=1689475213000"
-              title="green iguana"
-            />
-            <CardContent className="bg-[#b37eb5]">
-              <Typography gutterBottom variant="h5" component="div">
-                Introduction to Forex-learn to trade forex
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                "Introduction to Forex - Learn to Trade Forex" is a
-                comprehensive course designed to provide participants with
-                foundational knowledge and practical skills.
-              </Typography>
-            </CardContent>
-            <CardActions className="bg-[#b37eb5]">
-              <a
-                href="https://www.udemy.com/certificate/UC-977ceee6-810a-44e2-ba77-45335bfbb081/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button size="small">VIEW</Button>
-              </a>
-            </CardActions>
-          </Card>
-        </Grid>
+          {/* 4th certificate */}
+          <Grid item xs={12} md={4} className="flex justify-center">
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://udemy-certificate.s3.amazonaws.com/image/UC-977ceee6-810a-44e2-ba77-45335bfbb081.jpg?v=1689475213000"
+                title="green iguana"
+              />
+              <CardContent className="bg-[#b37eb5]">
+                <Typography gutterBottom variant="h5" component="div">
+                  Introduction to Forex-learn to trade forex
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  "Introduction to Forex - Learn to Trade Forex" is a
+                  comprehensive course designed to provide participants with
+                  foundational knowledge and practical skills.
+                </Typography>
+              </CardContent>
+              <CardActions className="bg-[#b37eb5]">
+                <a
+                  href="https://www.udemy.com/certificate/UC-977ceee6-810a-44e2-ba77-45335bfbb081/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button size="small">VIEW</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Grid>
 
-        {/* 4th certificate */}
-        <Grid item xs={12} md={4} className="flex justify-center">
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image={cert4}
-              title="Expanding the possibilities for the future success"
-            />
-            <CardContent className="bg-[#b37eb5]">
-              <Typography gutterBottom variant="h5" component="div">
-                Expanding the possibilities for the future success
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                "Spectrum of Opportunities: Expanding the Possibilities for
-                Future Success" encapsulates a transformative journey that
-                broadens horizons and opens doors to unparalleled opportunities.
-              </Typography>
-            </CardContent>
-            <CardActions className="bg-[#b37eb5]">
-              <a
-                href="https://drive.google.com/file/d/11kTtBNSVZcJjD4baUgQUrtNAgRNGZfC5/view?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button size="small">VIEW</Button>
-              </a>
-            </CardActions>
-          </Card>
+          {/* 4th certificate */}
+          <Grid item xs={12} md={4} className="flex justify-center">
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={cert4}
+                title="Expanding the possibilities for the future success"
+              />
+              <CardContent className="bg-[#b37eb5]">
+                <Typography gutterBottom variant="h5" component="div">
+                  Expanding the possibilities for the future success
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  "Spectrum of Opportunities: Expanding the Possibilities for
+                  Future Success" encapsulates a transformative journey that
+                  broadens horizons and opens doors to unparalleled
+                  opportunities.
+                </Typography>
+              </CardContent>
+              <CardActions className="bg-[#b37eb5]">
+                <a
+                  href="https://drive.google.com/file/d/11kTtBNSVZcJjD4baUgQUrtNAgRNGZfC5/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button size="small">VIEW</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </div>
   );
 }
