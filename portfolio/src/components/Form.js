a20a701247454c2863baeacdf86b0fb6b0c6a0dd
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-01-19 03:32:50 +0800

    Install: CoreUI
    Install CoreUI packages

--- portfolio/src/components/Form.js
+++ portfolio/src/components/Form.js
@@ -9,6 +9,11 @@ export default function Form(){
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState("");
 
+  const [state, setState] = useState(false);
+  const [stateO, setStateO] = useState(false);
+  const [stateG, setStateG] = useState(false);
+
+
 const handleSubmit = (e) => {
   e.preventDefault();
 
@@ -40,7 +45,6 @@ const handleSubmit = (e) => {
     return (
       <div>
         <form onSubmit={handleSubmit} className="emailForm">
-
           <input
             type="text"
             placeholder="sample@email.com"
@@ -64,9 +68,12 @@ const handleSubmit = (e) => {
             className="w-full h-48 bg-[#fff] border-[1px] border-[#ced4da] py-2 px-4 rounded-md mb-4 resize-none"
           ></textarea>
 
-          <button type="submit" className="px-4 py-1 w-full bg-[#b37eb5] font-bold font-maven text-[#fff] border-2 border-[#b37eb5] rounded-3xl mx-auto hover:bg-[#fff] hover:text-[#b37eb5] duration-500 md:mx-0 md:mr-3 2xl:text-[1.5rem] 2xl:px-6 2xl:py-4 2xl:rounded-[4em] ">
+          <CLoadingButton
+            type="submit"
+            className="px-4 py-1 w-full bg-[#b37eb5] font-bold font-maven text-[#fff] border-2 border-[#b37eb5] rounded-3xl mx-auto hover:bg-[#fff] hover:text-[#b37eb5] duration-500 md:mx-0 md:mr-3 2xl:text-[1.5rem] 2xl:px-6 2xl:py-4 2xl:rounded-[4em] "
+          >
             Send Message
-          </button>
+          </CLoadingButton>
         </form>
       </div>
     );
